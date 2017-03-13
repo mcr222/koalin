1495631345ed575bc953dd1437c9160e32c1f27a
mcr222 <markitus222@hotmail.com>
2017-03-13 19:25:12 +0100

    Added multiple tables

--- WebContent/process.php
+++ WebContent/process.php
@@ -1,8 +1,12 @@
 <?php
-	$data = $_POST['text'] . "\n";
-	echo $data;
+	$data = $_POST['text'];
+	$type = $_POST['type'];
+	
+	$fp = file('./data/data.txt');
+	$index = count($fp);
+	
 //IMPORTANT: with the server folder data/ should be given permission to write: sudo chmod -R 777 data/
-	$ret = file_put_contents('./data/data.txt', $data, FILE_APPEND | LOCK_EX);
+	$ret = file_put_contents('./data/data.txt', $index . ',' .  $type . ', ' . $data . "\n", FILE_APPEND | LOCK_EX);
 	header('Location: index.html'); 
 	exit()
 ?>
