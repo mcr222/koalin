027664a34a47e395ed9d5535204c312a6d5dfc47
mcr222 <markitus222@hotmail.com>
2017-03-14 00:57:52 +0100

    two tables and reply available plus styling

--- WebContent/process_reply.php
+++ WebContent/process_reply.php
@@ -1,12 +1,12 @@
 <?php
 	$data = $_POST['text'];
-	$type = $_POST['type'];
-	
+	$to_id = $_POST['to_id'];
+
 	$fp = file('./data/data.txt');
 	$index = count($fp);
 	
 //IMPORTANT: with the server folder data/ should be given permission to write: sudo chmod -R 777 data/
-	$ret = file_put_contents('./data/data.txt', $index . ',' .  $type . ',' . $data . ',0' . "\n", FILE_APPEND | LOCK_EX);
+	$ret = file_put_contents('./data/data.txt', $index . ',' .  "reply" . ',' . $data . ',' . $to_id . "\n", FILE_APPEND | LOCK_EX);
 	header('Location: index.html'); 
 	exit()
 ?>
